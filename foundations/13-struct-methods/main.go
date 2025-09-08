88fc72beec12f834a320891a89e0b76ef4142c18
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-09-07 23:11:39 -0300

    feat(foundations): interfaces

--- foundations/13-struct-methods/main.go
+++ foundations/13-struct-methods/main.go
@@ -9,6 +9,26 @@ type Endereco struct {
 	Estado     string
 }
 
+// Definindo uma interface
+// A interface nos possibilita definir um contrato que tipos concretos
+// devem seguir, garantindo que implementem certos métodos.
+// Qualquer struct que implemente o método Desativar, está implicitamente
+// satisfazendo a interface Pessoa.
+type Pessoa interface {
+	// A interface permite definir apenas métodos, não campos.
+	Desativar() // Método que deve ser implementado por qualquer tipo que satisfaça a interface
+}
+
+type Empresa struct {
+	Nome string
+}
+
+// Método Desativar associado ao tipo Empresa
+// Note que o receptor do método é uma cópia do Empresa (valor).
+// Portanto, alterações feitas em e dentro deste método não afetarão
+// a instância original.
+func (e Empresa) Desativar() {}
+
 type Cliente struct {
 	Nome     string
 	Idade    int
@@ -16,9 +36,18 @@ type Cliente struct {
 	Endereco Endereco
 }
 
+// Método Desativar associado ao tipo Cliente
+// Note que o receptor do método é uma cópia do Cliente (valor).
+// Portanto, alterações feitas em c dentro deste método não afetarão
+// a instância original.
 func (c Cliente) Desativar() {
 	c.Ativo = false
 	fmt.Printf("O cliente %s foi desativado\n", c.Nome)
+}
+
+// Função que aceita qualquer tipo que satisfaça a interface Pessoa
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
 }
 
 func main() {
@@ -28,7 +57,11 @@ func main() {
 		Idade: 24,
 		Ativo: true,
 	}
+	Desativacao(alexandre) // Passando o Cliente para a função que aceita a interface Pessoa
 
-	alexandre.Desativar()
+	minhaEmpresa := Empresa{
+		Nome: "Minha Empresa",
+	}
+	Desativacao(minhaEmpresa) // Passando a Empresa para a função que aceita a interface Pessoa
 
 }
