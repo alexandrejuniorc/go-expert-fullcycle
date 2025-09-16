1e08b302ccc579aee3676422c4866b180e0449e4
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-09-16 16:08:42 -0300

    feat(foundations): building project

---
+++ foundations/24-compiling-project/main.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+	a := 1
+	// b := 2
+	// c := 3
+
+	// Switch case utilizado para múltiplas condições
+	switch a {
+	case 1:
+		println("a é 1")
+	case 2:
+		println("a é 2")
+	case 3:
+		println("a é 3")
+	default:
+		println("a não é 1, 2 ou 3")
+
+	}
+
+	// Outra forma de utilizar o OR
+	// if a > b || c > a {
+	// 	println("a > b || c > a")
+	// }
+
+	// Outra forma de utilizar o AND
+	// if a > b && c > a {
+	// 	println("a > b && c > a")
+	// }
+
+	// If simples
+	// if a > b {
+	// 	println(a)
+	// } else {
+	// 	println(b)
+	// }
+}
